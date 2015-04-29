cb2db19c714f46dafdc8bd3676d26abc03d0b220
George Jiglau <george@mux.ro>
2015-04-30 01:20:02 +0300

    Redirect to mux.ro if not already there

--- mux.go
+++ mux.go
@@ -5,6 +5,13 @@ import "github.com/labstack/echo"
 func main() {
 	e := echo.New()
 
+	// Redirect all request to "mux.ro" if not already here
+	e.Use(func(c *echo.Context) {
+		if c.Request.Host != "mux.ro" {
+			c.Redirect(302, "http://mux.ro")
+		}
+	})
+
 	// Serve the stuff
 	e.Static("", "public")
 
